a11d2040d34422a86d04c4335e0da6114bf01fdf
Kamalesh V <117531507+KAMALESH0081@users.noreply.github.com>
2026-02-21 20:20:57 +0530

    Update solution for gradient-clipping

--- gradient-clipping/gradient-clipping.py
+++ gradient-clipping/gradient-clipping.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 def clip_gradients(g, max_norm):
-    g_1 = np.asarray(g)
+    g_1 = np.array(g)
         # Edge case: non-positive max_norm
     if max_norm <= 0:
         return g_1
-        
+    
     grad_norm = np.linalg.norm(g_1)
 
     if grad_norm > 0 and grad_norm > max_norm:
