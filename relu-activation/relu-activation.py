b0978321b28eb41fa20b0ecdf85fe262c491b05b
Kamalesh V <117531507+KAMALESH0081@users.noreply.github.com>
2026-02-27 13:59:45 +0530

    Update solution for relu-activation

--- relu-activation/relu-activation.py
+++ relu-activation/relu-activation.py
@@ -1,10 +1,4 @@
 import numpy as np
 
 def relu(x):
-    """
-    Implement ReLU activation function.
-    """
-    # Write code here
-    x = np.array(x)
-    x[x <= 0] = 0
-    return x
+    return np.maximum(0, x)
