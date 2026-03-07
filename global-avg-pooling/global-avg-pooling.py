af41c253de800824ca027bb1a47ae4d106d21356
Kamalesh V <117531507+KAMALESH0081@users.noreply.github.com>
2026-03-07 14:09:37 +0530

    Add solution for global-avg-pooling

---
+++ global-avg-pooling/global-avg-pooling.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def global_avg_pool(x):
+    """
+    Compute global average pooling over spatial dims.
+    Supports (C,H,W) => (C,) and (N,C,H,W) => (N,C).
+    """
+    x = np.array(x)
+
+    if x.ndim == 3:
+        result = np.mean(x, axis = (1, 2))
+    elif x.ndim == 4:
+        result = np.mean(x, axis = (2,3)) #.reshape(1,-1)
+    else:
+        raise ValueError("Value Error")
+    return result
